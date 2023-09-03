64a958558f9d0f945b328abaa4754b623729e8f3
Ruvilo <rubenvillaltalopez12@gmail.com>
2023-09-03 17:05:40 -0600

    feat(extra): add email input validator

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(
     () => {
 
-        //$("#checker").on('change', '#mailing-checkbox', populateData());
         $('#mailing-checkbox').change(
             function () {
                 if ($(this).is(':checked')) {
@@ -10,40 +9,57 @@ $(
             })
 
         $('#btn-submit').click(() => {
+            checkEmail()
             checkRequired()
-        });
+        })
         $('#btn-cancel').click(() => {
             cleanAll()
-        });
+        })
     }
 )
 
 const populateData = () => {
     const fields = ["-street-1", "-street-2", "-city", "-zip"]
     fields.map(field => {
-        console.log("#mailing" + field);
+        console.log("#mailing" + field)
         $("#mailing" + field).val($("#residential" + field).val())
     })
 }
 
 const checkRequired = () => {
     const ids = $('.check').map(function () {
-        return "#" + this.id;
+        return "#" + this.id
     }).get()
 
     ids.forEach(input => {
-        $(input).val() ? $(input).removeClass("error") :  $(input).addClass("error")
+        $(input).val() ? $(input).removeClass("error") : $(input).addClass("error")
     })
-    if($(':input','#form').hasClass("error")){
+    if ($(':input', '#form').hasClass("error")) {
         alert("Fill all required fields")
     }
-    console.log("submited")
+    else if (!checkEmail()) {
+        $("#email-address").addClass("error")
+        alert("Invalid email")
+
+    }
+    else {
+        alert("submitted")
+    }
 }
 
-const cleanAll=()=>{
-    $(':input','#form')
-    .not(':button, :submit, :reset, :hidden')
-    .val('')
-    .removeAttr('checked')
-    .removeAttr('selected');
+const cleanAll = () => {
+    $(':input', '#form')
+        .not(':button, :submit, :reset, :hidden')
+        .val('')
+        .removeAttr('checked')
+        .removeAttr('selected')
+
+    $("#mailing-checkbox").prop("checked", false)
+}
+
+const checkEmail = () => {
+    const email = $("#email-address").val()
+    const regex = /\S+@\S+\.\S+/
+
+    return email.match(regex)
 }
